b8c89ea4a606fd7f674151632c7bc467da3dcaf0
Kris Sankaran <sankaran.kris@gmail.com>
2020-07-23 00:12:50 -0500

    add global vars and remove unused funs

--- R/processing.R
+++ R/processing.R
@@ -1,33 +1,21 @@
 
-#' @importFrom dplyr %>% arrange %>%
-#' @importFrom tibble as_tibble
-unify_row_data <- function(row_data_list) {
-  do.call(rbind, row_data_list) %>%
-    as_tibble() %>%
-    unique() %>%
-    arrange(channel_name, marker_name)
-}
-
-#' @importFrom tibble as_tibble
-#' @importFrom SummarizedExperiment colData
-#' @importFrom dplyr bind_rows select everything mutate_at vars %>%
-unify_col_data <- function(col_data_list) {
-  col_union <- lapply(col_data_list, function(x) as_tibble(colData(x)))
-  bind_rows(col_union, .id = "cell_type") %>%
-    select(sample_id, starts_with("patient"), everything()) %>%
-    select(-file_name) %>%
-    mutate_at(vars(matches("Age|percent|Score")), as.numeric) %>%
-    mutate_at(vars(-matches("Age|percent|Score")), as_factor)
-}
-
-subsample_experiments <- function(x_list, p_keep = 0.05) {
-  for (i in seq_along(x_list)) {
-    D <- ncol(x_list[[i]])
-    sample_ix <- sample(D, D * p_keep, replace = FALSE)
-    x_list[[i]] <- x_list[[i]][, sample_ix]
-  }
-  x_list
-}
+#' removing notes
+#' https://stackoverflow.com/questions/9439256/how-can-i-handle-r-cmd-check-no-visible-binding-for-global-variable-notes-when
+#'
+globalVariables(c(
+  ".",
+  "SampleID",
+  "cellLabelInImage",
+  "channel_name",
+  "file_name",
+  "geometry",
+  "immune_group",
+  "marker_name",
+  "n_polys",
+  "sample_by_cell",
+  "sample_id",
+  "tumor_group",
+))
 
 #' Download Data for Experiments
 #'
